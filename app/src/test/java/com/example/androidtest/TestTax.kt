d04c7fc698b4ac74ce755e94e3286409b0b9c7d4
Berkay Sazak <berkaysazak313@gmail.com>
2022-09-14 18:22:36 +0300

    Truth Test

--- app/src/test/java/com/example/androidtest/TestTax.kt
+++ app/src/test/java/com/example/androidtest/TestTax.kt
@@ -1,18 +1,30 @@
 package com.example.androidtest
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class TestTax {
+
+    private lateinit var tax : Tax
+
+    @Before
+    fun setup(){
+        tax = Tax()
+    }
+    @After
+    fun teardown(){
+
+    }
+
     @Test
     fun calculateTaxTest(){
-        val tax = Tax()
         val netTax = tax.calculateTax(100.0,0.1)
         assertThat(netTax).isEqualTo(10)
     }
     @Test
     fun calculateIncomeTest(){
-        val tax = Tax()
         val netIncome = tax.calculateIncome(100.0,0.1)
         assertThat(netIncome).isEqualTo(90)
     }
